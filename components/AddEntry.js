5bb1ad345466d5313d2db01fe0505dd0ca889bca
Taynan Siqueira <taynacid123@gmail.com>
2019-01-24 15:17:30 -0300

    feat: component
    create a component for entrydata
    add a helper with initial data

---
+++ components/AddEntry.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import { getMetricMetaInfo } from "../utils/helpers";
+
+class AddEntry extends Component {
+  render() {
+    return <View>{getMetricMetaInfo("bike").getIcon()}</View>;
+  }
+}
+
+export default AddEntry;
