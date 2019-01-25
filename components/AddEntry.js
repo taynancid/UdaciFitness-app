1b2e18e88cf70f4fa6e883629c3f85e8b0425721
Taynan <taynacid123@gmail.com>
2019-01-25 19:48:03 -0200

    feat: create asyncstorage
    create asyncStorage as well a set and reset method

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import UdaciSteppers from "./UdaciSteppers";
 import DateHeader from "./DateHeader";
 import TextButton from "./TextButton";
 import { Ionicons } from "@expo/vector-icons";
+import { submitEntry, removeEntry } from "../utils/api";
 
 function SubmitBtn({ onPress }) {
   return (
@@ -69,10 +70,14 @@ class AddEntry extends Component {
         eat: 0
       };
     });
+
+    submitEntry({ key, entry });
   };
 
   reset = () => {
     const key = timeToString();
+
+    removeEntry(key);
   };
 
   render() {
