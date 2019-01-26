273a0832ef6815612e6d72bfd0bb58cd7f0ad014
Taynan <taynacid123@gmail.com>
2019-01-26 14:22:16 -0200

    feat: Style to slider, text button and dateheader
    add style to three more components

--- components/DateHeader.js
+++ components/DateHeader.js
@@ -1,9 +1,7 @@
-import React from 'react'
-import {View, Text} from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
+import { purple } from "../utils/colors";
 
-export default function UdaciSlider ({date}) {
-  return (
-    
-      <Text>{date}</Text>
-  )
+export default function UdaciSlider({ date }) {
+  return <Text style={{ color: purple, fontSize: 25 }}>{date}</Text>;
 }
