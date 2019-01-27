31e683ab7f8b811354e5236f28d344680eabcfa1
Taynan <taynacid123@gmail.com>
2019-01-27 11:35:28 -0200

    feat: Loading
    add a loading state to History component

--- components/History.js
+++ components/History.js
@@ -14,8 +14,13 @@ import UdaciFitnessCalendar from "udacifitness-calendar";
 import { white } from "../utils/colors";
 import DateHeader from "./DateHeader";
 import MetricCard from "./MetricCard";
+import { AppLoading } from "expo";
 
 class History extends Component {
+  state = {
+    ready: false
+  };
+
   componentDidMount() {
     const { dispatch } = this.props;
     fetchCalendarResults()
@@ -28,7 +33,12 @@ class History extends Component {
             })
           );
         }
-      });
+      })
+      .then(() =>
+        this.setState(() => ({
+          ready: true
+        }))
+      );
   }
 
   renderItem = ({ today, ...metrics }, formattedDate, key) => (
@@ -57,6 +67,11 @@ class History extends Component {
 
   render() {
     const { entries } = this.props;
+    const { ready } = this.state;
+
+    if (ready === false) {
+      return <AppLoading />;
+    }
 
     return (
       <UdaciFitnessCalendar
