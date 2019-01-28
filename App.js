cd51bae27a6e4ec289b9b17acfcbae4c10da6d0a
Taynan <taynacid123@gmail.com>
2019-01-28 10:48:04 -0200

    feat: Stack navigator
    add a stack navigator on top of the tab navigator

--- App.js
+++ App.js
@@ -2,10 +2,15 @@ import React from "react";
 import { View, Platform, StatusBar } from "react-native";
 import AddEntry from "./components/AddEntry";
 import History from "./components/History";
+import EntryDetail from "./components/EntryDetail";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
-import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import {
+  createBottomTabNavigator,
+  createStackNavigator,
+  createAppContainer
+} from "react-navigation";
 import { purple, white } from "./utils/colors";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import { Constants } from "expo";
@@ -18,41 +23,57 @@ function UdaciStatusBar({ backgroundColor, ...props }) {
   );
 }
 
-const Tabs = createBottomTabNavigator(
-  {
-    History: History,
-    AddEntry: AddEntry
-  },
-  {
-    navigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ tintColor }) => {
-        const { routeName } = navigation.state;
-        return routeName === "History" ? (
-          <FontAwesome name="home" size={30} color={tintColor} />
-        ) : (
-          <Ionicons name="ios-bookmarks" size={30} color={tintColor} />
-        );
+const MainNavigator = createStackNavigator({
+  Home: {
+    screen: createBottomTabNavigator(
+      {
+        History: History,
+        AddEntry: AddEntry
+      },
+      {
+        navigationOptions: ({ navigation }) => ({
+          tabBarIcon: ({ tintColor }) => {
+            const { routeName } = navigation.state;
+            return routeName === "History" ? (
+              <FontAwesome name="home" size={30} color={tintColor} />
+            ) : (
+              <Ionicons name="ios-bookmarks" size={30} color={tintColor} />
+            );
+          }
+        }),
+        tabBarOptions: {
+          showIcon: true,
+          activeTintColor: Platform.OS === "ios" ? purple : white,
+          style: {
+            height: 56,
+            backgroundColor: Platform.OS === "ios" ? white : purple,
+            shadowColor: "rgba(0,0,0,0.24)",
+            shadowOffset: {
+              width: 0,
+              height: 3
+            },
+            shadowRadius: 6,
+            shadowOpacity: 1
+          }
+        }
       }
-    }),
-    tabBarOptions: {
-      showIcon: true,
-      activeTintColor: Platform.OS === "ios" ? purple : white,
-      style: {
-        height: 56,
-        backgroundColor: Platform.OS === "ios" ? white : purple,
-        shadowColor: "rgba(0,0,0,0.24)",
-        shadowOffset: {
-          width: 0,
-          height: 3
-        },
-        shadowRadius: 6,
-        shadowOpacity: 1
+    ),
+    navigationOptions: {
+      header: null
+    }
+  },
+  EntryDetail: {
+    screen: EntryDetail,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple
       }
     }
   }
-);
+});
 
-const TabsContainer = createAppContainer(Tabs);
+const ScreensContainer = createAppContainer(MainNavigator);
 
 export default class App extends React.Component {
   render() {
@@ -60,7 +81,7 @@ export default class App extends React.Component {
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }}>
           <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
-          <TabsContainer />
+          <ScreensContainer />
         </View>
       </Provider>
     );
